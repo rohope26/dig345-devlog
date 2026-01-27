78a40536269f6ac76bebcb20f519f96beb00098d
rohope26 <137436931+rohope26@users.noreply.github.com>
2026-01-27 14:18:30 -0500

    GitHub pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import starlight from '@astrojs/starlight';
 // Astro config docs https://astro.build/config
 export default defineConfig({
 	// required to publish on GH Pages
-	site: 'https://omundy.github.io',
+	site: 'https://rohope26.github.io',
 	base: '/dig345-devlog',
 	integrations: [
 		// Startlight-specific config
 		starlight({
-			title: 'Owen\'s DevLog',
+			title: 'Ross\'s DevLog',
 			sidebar: [
 				{ label: 'Schedule', autogenerate: { directory: 'schedule' }, },
 			],
